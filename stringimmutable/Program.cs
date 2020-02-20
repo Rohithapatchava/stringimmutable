fcd25d894a866fb08b852343da1eebe9ad86fda4
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-20 16:29:33 +0530

    Initial commit

---
+++ stringimmutable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringimmutable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder str = new StringBuilder("hello");
+            Console.WriteLine(str);
+            str.Append(" world");
+            Console.WriteLine(str);
+            str.Insert(11, "good morning");
+            Console.WriteLine(str);
+            str.Remove(5, 6);
+            Console.WriteLine(str);
+            int i = str.Capacity;
+            Console.WriteLine(i);
+            str.Clear();
+            Console.WriteLine(str);
+        }
+    }
+}
